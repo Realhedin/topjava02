6072fa252c9a6f0edf6320296de04e5fea025c73
dkorolev <korwork87@yahoo.com>
2015-03-07 16:39:11 +0300

    added Spring annotations to UserMeal layers

--- src/main/java/ru/javawebinar/topjava/repository/UserMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/UserMealRepository.java
@@ -1,8 +1,42 @@
 package ru.javawebinar.topjava.repository;
 
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.UserMeal;
+
+import java.util.List;
+
 /**
  * GKislin
  * 06.03.2015.
  */
+@Repository
 public interface UserMealRepository {
+
+    /**
+     * save new meal
+     * @param meal
+     * @return
+     */
+    UserMeal save(UserMeal meal);
+
+    /**
+     * delete meal by id
+     * @param id
+     * @return
+     */
+    boolean delete(int id);
+
+    /**
+     * get meal by its id
+     * @param id
+     * @return
+     */
+    UserMeal getMeal(int id);
+
+    /**
+     * get all meals
+     * @return
+     */
+    List<UserMeal> getAllMeals();
+
 }
