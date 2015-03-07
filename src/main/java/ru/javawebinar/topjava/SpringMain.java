1975cfb9075957a9feb17cd59efbdd38b714aa90
dkorolev <korwork87@yahoo.com>
2015-03-07 17:15:43 +0300

    added MockUserMealRepository for test

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,9 +2,12 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.repository.mock.MockUserMealRepository;
 import ru.javawebinar.topjava.repository.mock.MockUserRepository;
 
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * User: gkislin
@@ -18,5 +21,16 @@ public class SpringMain {
         mockUserRepository = appCtx.getBean(MockUserRepository.class);
 
         appCtx.close();
+
+        //my check
+        System.out.println("\n\n");
+        try (ConfigurableApplicationContext appCtx2 = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println(appCtx2.getBean(MockUserMealRepository.class));
+            MockUserMealRepository mumr = appCtx2.getBean(MockUserMealRepository.class);
+            mumr.delete(3);
+            mumr.getAllMeals();
+            mumr.getMeal(3);
+            mumr.save(new UserMeal("myMeal",1000,new Date()));
+        }
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/MockUserMealRepository.java
@@ -0,0 +1,48 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.LoggerWrapper;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.repository.UserMealRepository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Dmitrii on 3/7/15.
+ */
+@Repository
+public class MockUserMealRepository implements UserMealRepository {
+
+    private static final LoggerWrapper LOG = LoggerWrapper.get(MockUserMealRepository.class);
+
+    @Override
+    public UserMeal save(UserMeal meal) {
+        LOG.info("Meal saved: " + meal);
+        return meal;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        LOG.info("Meal deleted");
+        return true;
+    }
+
+    @Override
+    public UserMeal getMeal(int id) {
+        UserMeal temp = new UserMeal("name",123,new Date());
+        LOG.info("Here is your meal: " + temp);
+        return temp;
+    }
+
+    @Override
+    public List<UserMeal> getAllMeals() {
+        List<UserMeal> list = new ArrayList<>();
+        list.add(new UserMeal("salad",1,new Date()));
+        list.add(new UserMeal("soup",2,new Date()));
+        list.add(new UserMeal("second dish",3,new Date()));
+        LOG.info("List is: " + list);
+        return list;
+    }
+}
