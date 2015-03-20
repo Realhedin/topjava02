f90120c385cc9a305844baa0dc09330c33d8010a
dkorolev <korwork87@yahoo.com>
2015-03-20 23:57:34 +0300

    after reset

---
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.web.meal.UserMealRestController;
+
+import java.util.Arrays;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public class SpringMain {
+    public static void main(String[] args) {
+        // java 7 Automatic resource management
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
+            UserMealRestController adminController = appCtx.getBean(UserMealRestController.class);
+            adminController.delete(7);
+        }
+    }
+}
