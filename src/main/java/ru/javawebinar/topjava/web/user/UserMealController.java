850eb122837ee4d0690e5d4d69b6eadf41e47d5c
dkorolev <korwork87@yahoo.com>
2015-04-08 22:08:34 +0300

    implement UserMealController.java for springMVC

---
+++ src/main/java/ru/javawebinar/topjava/web/user/UserMealController.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.javawebinar.topjava.model.BaseEntity;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.service.UserMealService;
+import ru.javawebinar.topjava.service.UserService;
+
+import java.util.List;
+
+/**
+ * Created by Dmitrii on 4/8/15.
+ */
+@Controller
+public class UserMealController {
+
+    @Autowired
+    UserMealService service;
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/meals", method = RequestMethod.GET)
+    public String getAllUserMeals(Model model) {
+        model.addAttribute("allMeals", service.getAll(BaseEntity.START_SEQ));
+        model.addAttribute("userName", userService.get(BaseEntity.START_SEQ).getName());
+        return "mealList";
+    }
+}
