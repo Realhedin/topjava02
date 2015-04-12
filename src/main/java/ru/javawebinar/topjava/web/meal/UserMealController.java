850655e8082d7ae812dc6fb08cb2e132b296af31
dkorolev <korwork87@yahoo.com>
2015-04-12 21:49:52 +0300

    getById test to UserMealController for SpringMVC

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.javawebinar.topjava.LoggedUser;
@@ -20,6 +21,12 @@ public class UserMealController {
     @RequestMapping(value = "/meals", method = RequestMethod.GET)
     public String mealList(Model model) {
         model.addAttribute("mealList", service.getAll(LoggedUser.id()));
+        return "mealList";
+    }
+
+    @RequestMapping(value = "/meals/{id}", method = RequestMethod.GET)
+    public String meal(@PathVariable("id") int id, Model model) {
+        model.addAttribute("meal", service.get(id,LoggedUser.id()));
         return "mealList";
     }
 }
--- src/test/java/ru/javawebinar/topjava/web/meal/UserMealControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealControllerTest.java
@@ -1,17 +1,12 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.BaseEntity;
 import ru.javawebinar.topjava.web.WebTest;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.hasProperty;
@@ -19,7 +14,9 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.Profiles.*;
+import static ru.javawebinar.topjava.Profiles.DATAJPA;
+import static ru.javawebinar.topjava.Profiles.POSTGRES;
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 /**
  * Created by Dmitrii on 4/12/15.
@@ -37,19 +34,31 @@ public class UserMealControllerTest extends WebTest {
                 .andExpect(model().attribute("mealList", Matchers.hasSize(4)))
                 .andExpect(model().attribute("mealList", Matchers.hasItem(
                         allOf(
-                                hasProperty("id", is(BaseEntity.START_SEQ + 2)),
+                                hasProperty("id", is(START_SEQ + 2)),
                                 hasProperty("description", is("breakfast"))
 
                         )
                 )))
                 .andExpect(model().attribute("mealList", Matchers.hasItem(
                         allOf(
-                                hasProperty("id", is(BaseEntity.START_SEQ+5)),
+                                hasProperty("id", is(START_SEQ+5)),
                                 hasProperty("dateTime", is(LocalDateTime.of(2015, 1, 7, 13, 0))),
                                 hasProperty("calories", is(1300))
                          )
                 )))
         ;
+    }
+
+
+    @Test
+    public void getById() throws Exception {
+        mockMvc.perform(get("/meals/{id}", BaseEntity.START_SEQ + 2))
+                .andExpect(status().isOk())
+                .andExpect(view().name("mealList"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/mealList.jsp"))
+                .andExpect(model().attribute("meal", hasProperty("id", is(START_SEQ + 2))))
+                .andExpect(model().attribute("meal", hasProperty("description", is("breakfast"))))
+                .andExpect(model().attribute("meal", hasProperty("dateTime", is(LocalDateTime.of(2015, 1, 6, 9, 0)))));
     }
 
 }
