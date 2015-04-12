10e12c0435ec69a5abe1f48ef6a029b2cc6a7fe6
dkorolev <korwork87@yahoo.com>
2015-04-12 20:37:14 +0300

    create UserMealControllerTest

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealControllerTest.java
@@ -0,0 +1,55 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.BaseEntity;
+import ru.javawebinar.topjava.web.WebTest;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.Profiles.*;
+
+/**
+ * Created by Dmitrii on 4/12/15.
+ */
+@ActiveProfiles(value = {POSTGRES, DATAJPA})
+public class UserMealControllerTest extends WebTest {
+
+    @Test
+    public void testUserMealList() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("mealList"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/mealList.jsp"))
+                .andExpect(model().attribute("mealList", Matchers.hasSize(4)))
+                .andExpect(model().attribute("mealList", Matchers.hasItem(
+                        allOf(
+                                hasProperty("id", is(BaseEntity.START_SEQ + 2)),
+                                hasProperty("description", is("breakfast"))
+
+                        )
+                )))
+                .andExpect(model().attribute("mealList", Matchers.hasItem(
+                        allOf(
+                                hasProperty("id", is(BaseEntity.START_SEQ+5)),
+                                hasProperty("dateTime", is(LocalDateTime.of(2015, 1, 7, 13, 0))),
+                                hasProperty("calories", is(1300))
+                         )
+                )))
+        ;
+    }
+
+}
