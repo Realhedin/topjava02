d82bdfcb42096f1675b52b4f87fb28146d927857
gkislin@yandex.ru <github13>
2015-04-13 01:00:46 +0300

    Fix tests: add hsqldb in maven test scope Remove mok create test Disable close spring context

--- src/test/java/ru/javawebinar/topjava/web/mock/UserAdminSpringMockTest.java
+++ src/test/java/ru/javawebinar/topjava/web/mock/UserAdminSpringMockTest.java
@@ -5,10 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
-import ru.javawebinar.topjava.web.user.AdminUserRestController;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 /**
  * GKislin
@@ -19,12 +17,7 @@ import ru.javawebinar.topjava.web.user.AdminUserRestController;
 public class UserAdminSpringMockTest {
 
     @Autowired
-    private AdminUserRestController controller;
-
-    @Test
-    public void testCreate() throws Exception {
-        controller.create(new User(null, "Name", "email@ya.ru", "password", true, Role.ROLE_USER));
-    }
+    private AdminRestController controller;
 
     @Test
     public void testDelete() throws Exception {
