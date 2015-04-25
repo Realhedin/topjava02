36eca404289bb3e9c0945f6fca193903da08825b
gkislin@yandex.ru <github13>
2015-04-25 15:30:01 +0300

    Switch to spring-security 4.01

--- src/test/java/ru/javawebinar/topjava/web/WebTest.java
+++ src/test/java/ru/javawebinar/topjava/web/WebTest.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.web;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -14,6 +13,8 @@ import ru.javawebinar.topjava.service.UserService;
 
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 /**
  * User: gkislin
@@ -37,12 +38,9 @@ abstract public class WebTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    @Autowired
-    private FilterChainProxy restSecurityFilterChain;
-
     @PostConstruct
     void postConstruct() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).addFilters(restSecurityFilterChain).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
     }
 
     @Before
